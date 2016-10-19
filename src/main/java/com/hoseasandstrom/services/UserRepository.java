9ca7dbc0a4cff8d08118ebab7792514c51480d69
hoseasandstrom <Lakyn4ever>
2016-10-19 14:00:16 -0400

    Setup basic login, logout and register routes

--- src/main/java/com/hoseasandstrom/services/UserRepository.java
+++ src/main/java/com/hoseasandstrom/services/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by hoseasandstrom on 10/19/16.
  */
 public interface UserRepository extends CrudRepository<User, Integer> {
+    public User findByUsername(String username);
 }
