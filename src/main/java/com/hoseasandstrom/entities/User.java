2aff561bc0931c04513c4d20e4cd9b90e5f018ed
hoseasandstrom <Lakyn4ever>
2016-10-19 12:44:42 -0400

    Added entities, controllers, seervices and utils

---
+++ src/main/java/com/hoseasandstrom/entities/User.java
@@ -0,0 +1,7 @@
+package com.hoseasandstrom.entities;
+
+/**
+ * Created by hoseasandstrom on 10/19/16.
+ */
+public class User {
+}
---
+++ src/main/java/com/hoseasandstrom/services/Person.java
@@ -0,0 +1,7 @@
+package com.hoseasandstrom.services;
+
+/**
+ * Created by hoseasandstrom on 10/19/16.
+ */
+public interface Person {
+}
