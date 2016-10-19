de92de99dda463689eefbc8e82d3986958faa84c
hoseasandstrom <Lakyn4ever>
2016-10-19 12:35:54 -0400

    Initial Setup

---
+++ src/main/java/com/hoseasandstrom/IMSSpringApplication.java
@@ -0,0 +1,12 @@
+package com.hoseasandstrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMSSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IMSSpringApplication.class, args);
+	}
+}
